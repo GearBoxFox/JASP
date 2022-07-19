a6512d78b15b396b6f4e59a82025fe89889f172a
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2022-07-19 11:40:01 -0400

    Revert "Inital Commit"
    This reverts commit d8efd8f9364c5153c81a34eb9b7f02ff4bccbb34.

--- TinkerRoom2d/Library/PackageCache/com.cammin.ldtkunity@3.0.3/Editor/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("LDtkUnity.Tests.EditMode")]
-[assembly: InternalsVisibleTo("LDtkUnity.Tests.PlayMode")]
--- TinkerRoom2d/Library/PackageCache/com.cammin.ldtkunity@3.0.3/Runtime/Data/Schema/Converter/Serialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace LDtkUnity
-{
-    public static class Serialize
-    {
-        public static string ToJson(this LdtkJson self) => JsonConvert.SerializeObject(self, LDtkUnity.Converter.Settings);
-    }
-}
